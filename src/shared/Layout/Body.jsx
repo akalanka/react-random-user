27e0d3017253cb0eb99c5c9264fa17d2eee3d806
akalanka <cakalanka@gmail.com>
2020-01-24 10:13:53 +0530

    Main layout configured

---
+++ src/shared/Layout/Body.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Body() {
+
+    return (
+        <div>Original Body</div>
+    );
+}
+
+export default Body;
---
+++ src/shared/Layout/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div>Original Header</div>
+    );
+}
+
+export default Header;
